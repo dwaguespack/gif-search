e9ec4c286c2c77cd951c708a8ca338175da02c0a
Dustin <dustinwaguespack@gmail.com>
2025-06-19 20:51:07 -0500

    copy works

--- src/App.js
+++ src/App.js
@@ -7,17 +7,29 @@ function App() {
   const [query, setQuery] = useState('');
   const [gifs, setGifs] = useState([]);
   const [limitModal, setLimitModal] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const [copiedId, setCopiedId] = useState(null);
+  const [timer, setTimer] = useState(null);
+  const [page, getPage] = useState(1);
+
 
   const searchGifs = async (e) => {
     e.preventDefault();
     if (!query) return;
     try {
       const response = await fetch(
-        `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&rating=g&q=${encodeURIComponent(query)}&limit=20`
+        `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&rating=g&q=${encodeURIComponent(query)}&limit=10`
       );
       if (response.ok) {
         const data = await response.json();
-        setGifs(data.data); 
+        // TODO: change this to 3
+        if(gifs.length === 1) {
+          // this only occurs when the app first loads
+          setGifs(data.data);
+        } else {
+          // add 10 new gifs to array
+        setGifs(gifs.concat(data.data)); 
+        }
       } else if(response.status === 429) {
         showLimitModal();
       } else {
@@ -29,13 +41,16 @@ function App() {
   };
 
   function showLimitModal() {
+    // show rate limit modal and close it after 8 seconds
     setLimitModal(true);
-    setTimeout(() => setLimitModal(false), 10000);
+    setTimer(setTimeout(() => setLimitModal(false), 8000));
   }
 
   useEffect(() => {
+    // populates view with initial 3 random gifs
     (async () => {
       try {
+        // TODO: call 2 more times
       const response = await fetch(
         `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&rating=g}`
       );
@@ -53,9 +68,21 @@ function App() {
     })()
   },[]);
 
+  function copyUrlToClipboard(gif) {
+    let url = gif.images.fixed_height.mp4;
+    navigator.clipboard.writeText(url).then(res => console.log(res));
+    setCopied(true);
+    setCopiedId(gif.id);
+    if(timer) {
+      clearTimeout(timer);
+    }
+    setTimer(setTimeout(() => setCopied(false), 8000));
+  };
+
   return (
     <div className="App">
       <h1>GIF Search</h1>
+      <p>Click on a GIF to copy its URL.</p>
       <form onSubmit={searchGifs}>
         <input
           type="text"
@@ -66,23 +93,26 @@ function App() {
         <button type="submit">Search</button>
       </form>
 
+      <div>{limitModal && (<div className="rate-limit-modal">GIPHY API rate limit reached!</div>)}</div>
+
       <div className="gif-grid">
         {gifs.map((gif) => (
-          <div key={gif.id} className="gif-item">
-            <video
-              src={gif.images.fixed_height.mp4}
-              autoPlay
-              loop
-              muted
-              playsInline
-              width="200"
-              height="200"
-            />
+          <div key={gif.id} className="gif-item" onClick={() => copyUrlToClipboard(gif)}>
+            <figure>
+              <video
+                src={gif.images.fixed_height.mp4}
+                autoPlay
+                loop
+                muted
+                playsInline
+                width="200"
+                height="200"
+              /> <figcaption>{(copied && gif.id === copiedId) ? 'Copied!' : gif.title}</figcaption>
+            </figure>
           </div>
         ))}
       </div>
-      <div>{limitModal && (<div className="rate-limit-modal">GIPHY API rate limit reached!</div>)}</div>
-      
+      <button class='more-button'>More</button>
     </div>
   );
 }
