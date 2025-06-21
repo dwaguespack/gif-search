699be7dc3a732edddb123f767b556254075c6ee7
Dustin <dustinwaguespack@gmail.com>
2025-06-20 19:03:49 -0500

    fixed gif grid conditional

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 const API_KEY = 've62PUc37AMEOfQHyEUJif2gVObK8SMg';
 
 function App() {
-  const [query, setQuery] = useState('');
-  const [gifs, setGifs] = useState([]);
-  const [limitModal, setLimitModal] = useState(false);
-  const [copied, setCopied] = useState(false);
-  const [copiedId, setCopiedId] = useState(null);
-  const [rateLimitTimer, setRateLimitTimer] = useState(null);
-  const [copiedTimer, setCopiedTimer] = useState(null);
-  const [page, setPage] = useState(0);
-  const [cache, setCache] = useState([]);
+  const [query, setQuery] = useState(''); // the query text
+  const [gifs, setGifs] = useState([]); // the gifs that appear in the view
+  const [limitModal, setLimitModal] = useState(false); // the API rate limit alert
+  const [copied, setCopied] = useState(false); // the copied GIF alert
+  const [copiedId, setCopiedId] = useState(null); // the ID of the copied GIF
+  const [rateLimitTimer, setRateLimitTimer] = useState(null); // the timer for the rate limit alert
+  const [copiedTimer, setCopiedTimer] = useState(null); // the timer for the copied alert
+  const [page, setPage] = useState(0); // the number used to paginate the display
+  const [cache, setCache] = useState([]); // the cached GIFs from all previous searches
 
 
   const searchGifs = async (e) => {
@@ -26,15 +26,15 @@ function App() {
       return;
     }
     try {
+      // the free GIPHY API key allows max limit=50
       const response = await fetch(
-        `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&rating=g&q=${encodeURIComponent(query)}&limit=100`
+        `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&rating=g&q=${encodeURIComponent(query)}&limit=50`
       );
       if (response.ok) {
         const data = await response.json();
         setGifs(data.data);
-        console.log(gifs.length);
-        let cacheItem = {'query':query, 'data': data.data};
-        updateCache(cacheItem);
+        let cacheItem = {'query':query, 'data': data.data, 'offset':1};
+        addToCache(cacheItem);
         setPage(0);
       } else if(response.status === 429) {
         showLimitModal();
@@ -44,13 +44,13 @@ function App() {
     } catch (err) {
       console.error('Error fetching GIFs:', err);
     }
-  };
+  }
 
   function searchCache(query) {
     return cache.find((item) => item.query === query);
   }
 
-  function updateCache(cacheItem) {
+  function addToCache(cacheItem) {
     setCache([...cache, cacheItem]);
   }
 
@@ -89,15 +89,20 @@ function App() {
   function copyUrlToClipboard(gif) {
     let url = gif.images.fixed_height.mp4;
     navigator.clipboard.writeText(url).then(res => console.log(res));
+    // changes the GIF text to 'Copied!' for 6 seconds
     setCopied(true);
     setCopiedId(gif.id);
     if(copiedTimer) {
       clearTimeout(copiedTimer);
     }
     setCopiedTimer(setTimeout(() => setCopied(false), 6000));
-  };
+  }
 
-  function handleNext() {
+  async function handleNext(e) {
+    e.preventDefault();
+    if((page+1)*10 >= gifs.length) {
+      await requestMoreGifs();
+    }
     setPage(page + 1);
   }
 
@@ -110,6 +115,35 @@ function App() {
     setQuery(query);
     setPage(0);
     setGifs([...cacheItem.data]);
+  }
+
+  async function requestMoreGifs() {
+    // call endpoint with offset
+    let cacheItem = searchCache(query);
+    try {
+      const response = await fetch(
+        `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&rating=g&q=${encodeURIComponent(query)}&limit=50&offset=${cacheItem.offset*50}`
+      );
+      if (response.ok) {
+        const data = await response.json();
+        let newData = [...cacheItem.data, ...data.data];
+        setGifs(newData);
+        updateCacheItem({'query':query, 'data':newData, 'offset': cacheItem.offset + 1});
+      } else if(response.status === 429) {
+        showLimitModal();
+      } else {
+        throw new Error('Response was not ok');
+      }
+    } catch (err) {
+        console.error('Error fetching GIFs:', err);
+    }
+  }
+
+  function updateCacheItem(newCacheItem) {
+    let index = cache.findIndex((item) => item.query === newCacheItem.query);
+    let newCache = [...cache];
+    newCache[index] = newCacheItem;
+    setCache(newCache);
   }
 
   return (
@@ -134,6 +168,7 @@ function App() {
 
       <div className="gif-grid">
         {gifs.map((gif, index) => (
+          ((index >= page*10) && (index < (page+1)*10)) && (
           <div key={gif.id} className="gif-item" onClick={() => copyUrlToClipboard(gif)} hidden={!((index >= page*10) && (index < (page+1)*10))}>
             <figure>
               <video
@@ -145,7 +180,7 @@ function App() {
               /> <figcaption>{(copied && gif.id === copiedId) ? 'Copied!' : gif.title}</figcaption>
             </figure>
           </div>
-        ))}
+        )))}
       </div>
       <button class='nav-button' onClick={hanldePrevious} hidden={page === 0}>&lt;&lt; Previous</button>
       <button class='nav-button' onClick={handleNext}>Next &gt;&gt;</button>
